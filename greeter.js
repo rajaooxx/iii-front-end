329a38b0d3f4123031f4ea1bf6b4bf5410215bbb
rajaooxx <rajaooxx@gmail.com>
2018-06-11 12:18:13 +0800

    greeter

--- greeter.js
+++ greeter.js
@@ -7,5 +7,5 @@
 // For each line FOO received on STDIN, respond with "Hello FOO!".
 while (true) {
   var input= WScript.stdIn.readLine();
-  WScript.echo('Hello ' + input+ '!');
+  WScript.echo('Hi ' + input+ '!');
 }
